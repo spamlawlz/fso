24ad071976996a4b799f9fbd4a654940e5ea704b
Sam Lawler <sam.lawler@gmail.com>
2023-08-22 12:20:28 -0700

    exercise 1.2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,3 +1,7 @@
+const Part = (props) => {
+  return <p>{props.part} {props.exercises}</p>
+}
+
 const Header = (props) => {
   return <h1>{props.course}</h1>
 }
@@ -5,15 +9,9 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <div>
-        <p>
-          {props.part1} {props.exercises1}
-        </p>
-        <p>
-          {props.part2} {props.exercises2}
-        </p>
-        <p>
-        {props.part3} {props.exercises3}
-      </p>
+        <Part part={props.part1} exercises={props.exercises1} />
+        <Part part={props.part2} exercises={props.exercises2} />
+        <Part part={props.part3} exercises={props.exercises3} />
     </div>
   )
 
